d405451207d69e42276fe4a0253b78b6a4baca30
bouabdallah93 <bouabdallah93@gmail.com>
2019-02-12 19:57:40 +0100

    mon cv

---
+++ jquery/jquery.js
@@ -0,0 +1,1 @@
+<script charset="utf-8" src="http://code.jquery.com/jquery-X.Y.Z.min.js"></script>
---
+++ jquery/script.js
@@ -0,0 +1,15 @@
+$(function() {
+
+	
+	$("#start").click(function(){
+	$("#p1").siblings().css("border-color", "green");
+
+		
+	});
+
+	//$("#but2").click(function(){
+	//$("#p1").removeClass("red");
+	//});
+	
+
+});
